ba1298dab5ff9f12978c36a4250e245efa5549bb
brian <44350146+brianf203@users.noreply.github.com>
2024-01-12 19:48:26 -0500

    created template for prediction page

--- src/Prediction.jsx
+++ src/Prediction.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Prediction.css';
 
 const Prediction = () => {
   return (
-    <div>
-      <h1>Prediction Results</h1>
-      {
-
-      }
+    <div className="prediction-container">
+      <div className="box top-left">Sunset: 5:45</div>
+      <div className="box top-center">Prediction</div>
+      <div className="box top-right">Statistics</div>
+      <div className="box middle-left">Text 4</div>
+      <div className="box middle-center image-box">Image</div>
+      <div className="box middle-right">Text 5</div>
+      <div className="box bottom-left">Text 6</div>
+      <div className="box bottom-center">Text 7</div>
+      <div className="box bottom-right">Text 8</div>
     </div>
   );
 };
